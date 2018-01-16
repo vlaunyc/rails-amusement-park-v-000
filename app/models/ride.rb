7d447e11ff72c00587d624ede76e07105bfe824e
Vicky Lau <vlau.nyc@gmail.com>
2018-01-16 23:29:23 +0000

    All model specs passing.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,20 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if user.too_broke(attraction) && user.too_short(attraction)
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.too_broke(attraction)
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.too_short(attraction)
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  def too_broke(attraction)
+    self.tickets < attraction.tickets
+  end
+
+  def too_short(attraction)
+    self.height < attraction.min_height
+  end
+
+  def mood
+    if nausea > happiness
+      "sad"
+    else
+      "happy"
+    end
+  end
 end
